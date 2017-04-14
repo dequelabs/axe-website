b5ee17e231a1dd4cc963cfc135c1c0ede5d598be
Wilco Fiers <wilco.fiers@deque.com>
2017-04-14 12:44:31 +0200

    use codeflask

--- assets/scripts/axe-playground.js
+++ assets/scripts/axe-playground.js
@@ -1,4 +1,16 @@
-jQuery(function ($) {
+document.addEventListener('DOMContentLoaded', function () {
+  var $ = jQuery
+
+  var markup = new CodeFlask();
+  markup.run('#markup', { language: 'html', lineNumbers: false });
+  var options = new CodeFlask();
+  options.run('#playground-config', { language: 'javascript', lineNumbers: false });
+
+  $.getScript('/assets/scripts/playground.js', function () {
+    markup.update()
+    options.update()
+  });
+
   $('#toggle-input, #apply-preset, #show-options').on('click', function () {
     $('#markup-input').toggle();
     $('#markup-select').toggle();
